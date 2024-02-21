d1e517aa9c8168cc36f5e4237e175d9ddbdcae4f
Greg <gregthompson27@gmail.com>
2024-02-21 12:17:28 -0600

    add conditionals to prevent errors for students that don't have repo

--- pull-all.sh
+++ pull-all.sh
@@ -1,22 +1,28 @@
 for i in $(cat < "list.txt"); do
-    name=$(echo $i | tr -d " \t\n\r")
-    echo $name
-    cd $name
-    git pull --ff
-    cd ..
+    if test -e $i; then
+        name=$(echo $i | tr -d " \t\n\r")
+        echo $name
+        cd $name
+        git pull --ff
+        echo ""
+        cd ..
+    fi
 done
 
-echo "\n"
+echo ""
 echo "---------"
-echo "\n"
+echo "❗❗❗❗❗❗❗❗❗❗❗❗❗❗❗"
 echo "commits within the last hour are below:"
 
 for i in $(cat < "list.txt"); do
-    name=$(echo $i | tr -d " \t\n\r")
-    echo $name
-    cd $name
-    PAGER="/bin/cat"
-    git log --since="1 hour ago" --pretty=format:"^^^^^ committed at %cd" -1
-    echo "\n"
-    cd ..
+    if test -e $i; then
+        name=$(echo $i | tr -d " \t\n\r")
+        echo $name
+        cd $name
+        PAGER="/bin/cat"
+        git log --since="1 hour ago" --pretty=format:"^^^^^ committed at %cd" -1
+        echo ""
+        echo ""
+        cd ..
+    fi
 done
